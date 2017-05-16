36ba385f15e3e8d906d90bc3ed1157c51294837f
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-05-15 23:13:41 -0300

    Created List components

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList
+} from 'react-native';
+
+class List extends Component {
+  render(){
+    <View>
+      <FlatList />
+    </View>
+  }
+}
